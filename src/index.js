9c8dd08d4212586c3355bb7efbcb18bf705b95c6
Jose Recart <joserecart91@gmail.com>
2022-09-21 12:27:51 -0300

    finished

--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
 // Random Burger Generator
 // url: c
-const beerDisplay = document.querySelector
+const beerDisplay = document.querySelector('.name')
+const button = document.querySelector('.button')
 //GET
-fetch('http://my-burger-api.herokuapp.com/burgers')
-    .then(response => response.json())
-    .then(data => {
-        console.log(data)
-        const name =  data[0].name
-        console.log(name)
+function getData() {
+    fetch('http://my-burger-api.herokuapp.com/burgers')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
 
-        
-    })
+            const randomNumber = Math.random() * data.length
+            const randomInteger =  Math.floor(randomNumber)
+            const name =  data[randomInteger].name
+            console.log(name)
+            beerDisplay.innerHTML = name
+
+            
+        })
+}
+
+button.addEventListener('click', getData)
         
         
